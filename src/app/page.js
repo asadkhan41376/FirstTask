40a65cfb2df657f4cfedbe8e29f9b371cdbdc727
mohammad asad <asadkhan41376@gmail.com>
2024-12-19 17:06:28 +0530

    some isshue sol

--- src/app/page.js
+++ src/app/page.js
@@ -11,16 +11,16 @@ const page = () => {
   return (
     <div className=' flex flex-col justify-center items-center  bg-[url("/myImg/construction.jpg")] bg-no-repeat bg-cover  relative pb-6'>
      <div className="absolute w-full h-[100%] top-0 left-0 bg-[#01010165] z-[0] " ></div>
-      <div className='pt-[10px]'> 
-         <h1 className='text-3xl  rounded-md text-[#003366]'>Reward Chart</h1>
+      <div className='pt-[10px] z-10'> 
+         <h1 className='text-3xl  rounded-md text-[#FCC737] '>Reward Chart</h1>
       </div>
       {/* ========================= */}
-      <div className="flex justify-between items-center w-[69%] py-3 ">
+      <div className="flex justify-between items-center w-[68%] py-3 ">
         <div className=' text-white  px-2 rounded-lg'>
 
           {/* name input =================== */}
           <div className="flex w-full  items-center relative gap-1 text-black ">
-            <label htmlFor="Name" className='text-[#003366] text-lg mr-2'>
+            <label htmlFor="Name" className='text-[#FCC737] text-lg mr-2'>
               Name
             </label>
             <Input type="text" id="Name" placeholder="Your Name" className=" bg-transparent   placeholder:text-black placeholder:text-lg   border-white rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:border-gray-300 relative z-10" />
@@ -36,7 +36,7 @@ const page = () => {
 
         {/* Date input ================== */}
         <div className="flex items-center relative ">
-          <label htmlFor="date" className='text-[#003366] text-lg mr-2'>
+          <label htmlFor="date" className='text-[#FCC737] text-lg mr-2'>
             Date
           </label>
           <Input type="date" id="Date" placeholder="2-10-2024" className=" bg-transparent   placeholder:text-black placeholder:text-lg   border-white rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-blue-500 hover:border-gray-300 relative z-10" />
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,16 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
 
 import { Checkbox } from "@/components/ui/checkbox"
 const Table = () => {
+
+const Box = useRef(null)
+const handleImag =()=>{
+  
+  
+  
+}
+
   return (
     < >
 
@@ -13,8 +22,8 @@ const Table = () => {
               <h1 className='text-md rounded-lg  font-bold text-center border w-[200px] h-[35px] border-black  bg-[#FCC737] ' ></h1>
             </div>
 
-            <div className=" flex gap-5">
-              <Checkbox className="w-[35px] h-[30px] " />
+            <div className=" flex gap-5" >
+              <Checkbox className="w-[35px] h-[30px] " ref={Box} onClick={handleImag} />
               <Checkbox className="w-[35px] h-[30px] " />
               <Checkbox className="w-[35px] h-[30px] " />
               <Checkbox className="w-[35px] h-[30px] " />
